cf4c0fc45271c73a73314f150f7655cd66eb0408
Alin chakma <chakmaalin54@gmail.com>
2025-07-15 01:48:24 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import os
 import time
+import sys
+
+# Hack Style Typing Animation
+def hacker_typing(text, delay=0.02):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
+
+# MATRIX Effect (optional)
+os.system("cmatrix -b -u 3 &")
+time.sleep(2)
+os.system("pkill cmatrix")
 
 # ফেসবুক আইডি ওপেন
-print("\n\033[1;32m[✓] Redirecting to Facebook profile...\033[0m\n")
-time.sleep(1)
+hacker_typing("\n[✓] Redirecting to Facebook profile...\n", 0.05)
 os.system("xdg-open https://www.facebook.com/profile.php?id=100001889177878")
+time.sleep(1)
 
 # স্ক্রিন পরিষ্কার
 os.system("clear")
@@ -31,13 +45,11 @@ elif choice == '2' or choice == '02':
     os.system("python logisystem.py")
 
 elif choice == '3' or choice == '03':
-    print("\n\033[1;33mOpening Facebook...\033[0m\n")
-    time.sleep(1)
+    hacker_typing("\nOpening Facebook...\n", 0.05)
     os.system("xdg-open https://www.facebook.com/profile.php?id=100001889177878")
 
 elif choice == '4' or choice == '04':
-    print("\n\033[1;31mExiting Tools...\033[0m\n")
-    time.sleep(1)
+    hacker_typing("\nExiting Tools...\n", 0.05)
     os.system("exit")
 
 else:
